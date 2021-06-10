ab596779a1c3d3ae8f6967f5a701bfba0bca69a1
Katherine Yevsukov <katherineyevsukov@iMac.local>
2021-06-10 17:49:58 -0400

    finished task 5

--- index.js
+++ index.js
@@ -99,8 +99,9 @@ Reyna's feedback is missing! Use what you know to do the following: (no function
   2. log the reviews array to the console to check your work
 */
 
+reviews[7].feedback = 'this place is chill with really cool people, great for getting work done on weekdays'
 
-
+console.log(reviews[7])
 
 
 /* 🚀🚀🚀🚀🚀🚀🚀🚀🚀🚀 Task 6: 🚀🚀🚀🚀🚀🚀🚀🚀🚀🚀
